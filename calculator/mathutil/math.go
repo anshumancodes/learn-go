0d6b4b57db42644c41dbc57190f1f869b0437d4a
anshumancodes <anshumanprof01@gmail.com>
2026-07-01 20:58:19 +0530

    feat(mathutil): add basic arithmetic functions

---
+++ calculator/mathutil/math.go
@@ -0,0 +1,17 @@
+package mathutil
+
+func Add(a, b int) int {
+
+	return a + b
+
+}
+
+func Subtract(a, b int) int {
+	return a - b
+}
+func Divide(a, b int) float32 {
+	return float32(a) / float32(b)
+}
+func Multiply(a, b int) float32 {
+	return float32(a) * float32(b)
+}
