0cc08263a08803237e7623167bd044512e5d6efb
anshumancodes <anshumanprof01@gmail.com>
2026-07-01 20:58:31 +0530

    feat(calculator): add basic calculator with mathutil

---
+++ calculator/go.mod
@@ -0,0 +1,3 @@
+module github.com/anshumancodes/calculator
+
+go 1.26.4
---
+++ calculator/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "github.com/anshumancodes/calculator/mathutil"
+func main() {
+	fmt.Println(mathutil.Add(2,3))
+	fmt.Println(mathutil.Subtract(2,3))
+	fmt.Println(mathutil.Divide(4,2))
+	fmt.Println(mathutil.Multiply(2,10))
+
+}
