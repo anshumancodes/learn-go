6c7510499d182b7ab38e8ab1fd54ca689849e91e
anshumancodes <anshumanprof01@gmail.com>
2026-09-15 13:53:53 +0530

    feat(basics): add time and switch examples

--- basics/main.go
+++ basics/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"runtime"
+	"time"
 )
 
 func main() {
@@ -17,26 +18,37 @@ func main() {
 	// type conversion & arithmetic
 	var z int32 = 2
 	var zInt int = int(z)
-	fmt.Printf("%v\n", i + zInt)
+	fmt.Printf("%v\n", i+zInt)
 
-	// loop 
+	// loop
 	// go has only for loop
 
-	sum:=0
-	for y:=0;y<10;y++{
-		sum+=y
+	sum := 0
+	for y := 0; y < 10; y++ {
+		sum += y
 	}
 	fmt.Println(sum)
 
 	// switch case in go
-	// 
-	switch os:=runtime.GOOS;os{
-		case "darwin":
-			fmt.Println("mac")
-		case "linux":
-			fmt.Println("linux")
-		default:
-			fmt.Println("unknown")
-			fmt.Printf("%s.\n",os)
+	//
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("mac")
+	case "linux":
+		fmt.Println("linux")
+	default:
+		fmt.Println("unknown")
+		fmt.Printf("%s.\n", os)
+	}
+	// another pattern of switch case
+	t := time.Now()
+	fmt.Println(t.Hour())
+	switch {
+	case t.Hour() < 12:
+		fmt.Println(" good morning!")
+	case t.Hour() < 17:
+		fmt.Println("good afternoon!")
+	default:
+		fmt.Println("good evening!")
 	}
 }
