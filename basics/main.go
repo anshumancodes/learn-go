49e476ce9b6a97a66060720803a4fe31fadc26d7
anshumancodes <anshumanprof01@gmail.com>
2026-09-15 13:49:48 +0530

    feat(basics): add examples for var, loop, and switch

--- basics/main.go
+++ basics/main.go
@@ -3,13 +3,40 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"runtime"
 )
 
 func main() {
-	var i=10
+	// variables
+	var i = 10
+	j := 6
+	// using variables & fmt package
 	fmt.Print(i)
-	fmt.Println("initilising a shardcache!",rand.Int())
-	var z int32=2
+	fmt.Print(j)
+	fmt.Println("initilising a shardcache!", rand.Int())
+	// type conversion & arithmetic
+	var z int32 = 2
 	var zInt int = int(z)
-	fmt.Print(i+zInt)
+	fmt.Printf("%v\n", i + zInt)
+
+	// loop 
+	// go has only for loop
+
+	sum:=0
+	for y:=0;y<10;y++{
+		sum+=y
+	}
+	fmt.Println(sum)
+
+	// switch case in go
+	// 
+	switch os:=runtime.GOOS;os{
+		case "darwin":
+			fmt.Println("mac")
+		case "linux":
+			fmt.Println("linux")
+		default:
+			fmt.Println("unknown")
+			fmt.Printf("%s.\n",os)
+	}
 }
